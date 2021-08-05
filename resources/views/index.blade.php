f1e3adcca8ca70f081479ca8997e2f1d09281508
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-05 17:14:03 +0100

    Removed asset helper.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     <title>Susscode | Home</title>
 </head>
 <body class="dark:bg-gray-700">
