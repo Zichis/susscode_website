5fda25ff67e7d38608a457c9ae3aba0533ad7aff
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-05 16:38:34 +0100

    Initial page design and structure.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Susscode | Home</title>
+</head>
+<body class="dark:bg-gray-700">
+    <nav class="border-b dark:border-gray-600">
+        <div class="flex justify-between items-center px-5 md:px-12 lg:px-24 py-2">
+            <div class="flex items-center">
+                <img src="{{ asset('images/susscode_logo.svg') }}" class="w-14 rounded md:w-20" alt="logo">
+                <h3 class="ml-3 text-3xl text-yellow-500 font-play">
+                    Susscode
+                </h3>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div class="text-center py-10 md:py-20 px-5">
+            <h3 class="text-3xl md:text-4xl lg:text-5xl my-3 md:my-5 text-gray-500 dark:text-gray-300">We write the</h3>
+            <h2 class="text-6xl md:text-7xl lg:text-8xl my-3 md:my-5 text-gray-600 dark:text-gray-100 font-play">Susscode</h2>
+            <h3 class="text-3xl md:text-4xl lg:text-5xl my-3 md:my-5 text-green-400">You have a Software!</h3>
+            <img src="{{ asset('images/susscoder.jpg') }}" class="w-full md:w-1/3 mx-auto my-10 rounded shadow-lg" alt="developer">
+        </div>
+    </header>
+</body>
+</html>
