1c919726390f658f200792732e71e9aae5398391
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-06 14:56:51 +0100

    Added route for contact us.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactUs;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::post('/contact-us', ContactUs::class)->name('contact-us');
