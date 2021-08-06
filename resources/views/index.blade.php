8eb9e97bfa93765e5a2a6c6e553d2712defe4813
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-06 11:35:23 +0100

    Changed sentence.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
         <div class="text-center py-10 md:py-20 px-5">
             <h3 class="text-3xl md:text-4xl lg:text-5xl my-3 md:my-5 text-gray-500 dark:text-gray-300">We write the</h3>
             <h2 class="text-6xl md:text-7xl lg:text-8xl my-3 md:my-5 text-gray-600 dark:text-gray-100 font-play">Susscode</h2>
-            <h3 class="text-3xl md:text-4xl lg:text-5xl my-3 md:my-5 text-green-500">You have a Software!</h3>
+            <h3 class="text-3xl md:text-4xl lg:text-5xl my-3 md:my-5 text-green-500">You own the Software!</h3>
             <img src="{{ asset('images/susscoder.jpg') }}" class="w-full md:w-1/3 mx-auto my-10 rounded shadow-lg" alt="developer">
         </div>
     </header>
