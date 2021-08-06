8df5687e4c341c070aeff595f581d1044d0759a4
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-06 16:00:49 +0100

    Updated page with styles and alert.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,14 @@
     <link href="/css/app.css" rel="stylesheet">
     <title>Susscode | Home</title>
 </head>
-<body class="dark:bg-gray-700">
+<body class="bg-gray-100 dark:bg-gray-700">
+    @include('sweetalert::alert')
     <nav class="border-b bg-green-500 dark:border-gray-600 dark:bg-transparent">
         <div class="flex justify-between items-center px-5 md:px-12 lg:px-24 py-2">
             <div class="flex items-center">
                 <img src="{{ asset('images/logopng.png') }}" class="w-12 h-14 rounded md:w-12" alt="logo">
             </div>
+            <a href="#contact-us" class="text-yellow-500 hover:text-yellow-400">Contact Us</a>
         </div>
     </nav>
     <header>
@@ -23,7 +25,13 @@
             <img src="{{ asset('images/susscoder.jpg') }}" class="w-full md:w-1/3 mx-auto my-10 rounded shadow-lg" alt="developer">
         </div>
     </header>
-    <section class="p-10" style="background: rgba(0, 0, 0, 0.3)">
+    <section class="px-10 py-20 bg-white dark:bg-gray-400 text-center">
+        <h3 class="text-3xl md:text-4xl lg:text-5xl text-center text-yellow-400">What We Do</h3>
+        <p class="py-3 w-full md:w-2/3 lg:w-1/3 text-2xl font-thin mx-auto dark:text-gray-100">We find a problem and curate the solution. Modern day problems
+            require modern day solutions and Susscode is the home of modern day Software solutions.
+        </p>
+    </section>
+    <section class="p-10" style="background: rgba(0, 0, 0, 0.3)" id="contact-us">
         <h3 class="text-3xl md:text-4xl lg:text-5xl text-center text-gray-100">Contact Us</h3>
         <div class="my-10 md:flex">
             <div class="text-white md:text-left md:w-1/2">
@@ -57,5 +65,10 @@
             </form>
         </div>
     </section>
+    <footer>
+        <div class="text-center bg-gray-300 dark:bg-gray-900 p-5">
+            <p class="text-gray-600 dark:text-gray-500">&copy; {{ date("Y") }} Susscode. All rights reserved.</p>
+        </div>
+    </footer>
 </body>
 </html>
