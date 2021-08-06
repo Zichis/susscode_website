d642aa13573bbd503b4dba371a7fbe59443a9912
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-08-06 14:58:00 +0100

    Added design for index page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,5 +23,39 @@
             <img src="{{ asset('images/susscoder.jpg') }}" class="w-full md:w-1/3 mx-auto my-10 rounded shadow-lg" alt="developer">
         </div>
     </header>
+    <section class="p-10" style="background: rgba(0, 0, 0, 0.3)">
+        <h3 class="text-3xl md:text-4xl lg:text-5xl text-center text-gray-100">Contact Us</h3>
+        <div class="my-10 md:flex">
+            <div class="text-white md:text-left md:w-1/2">
+                <p class="mb-2">Tel: +234 810 899 0031</p>
+                <p class="mb-2">Email: info@susscode.com</p>
+            </div>
+            <form action="{{ route('contact-us') }}" method="POST" class="md:text-left md:w-1/2">
+                @csrf
+                <div class="mb-3">
+                    <label class="text-yellow-300" for="name">Name</label>
+                    <input class="w-full px-2 py-1 rounded" type="text" name="name" id="name" placeholder="Enter your name" required>
+                    @error('name')
+                        <p class="text-red-400 my-1">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label class="text-yellow-300" for="name">Email</label>
+                    <input class="w-full px-2 py-1 rounded" type="email" name="email" id="email" placeholder="Enter your email" required>
+                    @error('email')
+                        <p class="text-red-400 my-1">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label class="text-yellow-300" for="name">Message</label>
+                    <textarea class="w-full px-2 py-1 rounded" name="message" id="message" rows="5" placeholder="Enter your message" required></textarea>
+                    @error('message')
+                        <p class="text-red-400 my-1">{{ $message }}</p>
+                    @enderror
+                </div>
+                <button class="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-400" type="submit">Send</button>
+            </form>
+        </div>
+    </section>
 </body>
 </html>
